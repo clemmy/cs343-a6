91f31d79c81521357689362324163172263c8d61
Clement Hoang <clement.hoang24@gmail.com>
2016-12-01 21:27:02 -0500

    Add Groupoff

--- Groupoff.h
+++ Groupoff.h
@@ -1,11 +1,25 @@
-#ifndef GROUPOFF_H
-#define GROUPOFF_H
+#ifndef __GROUPOFF_H__
+#define __GROUPOFF_H__
+
+#include "RNG.h"
+#include "Watcard.h"
+#include "Printer.h"
+#include <vector>
+
+using namespace std;
 
 _Task Groupoff {
+    Printer &printer;
+    unsigned int numStudents;
+    unsigned int sodaCost;
+    unsigned int groupOffDelay;
+    vector<WATCard::FWATCard> unusedWatCards;
+    vector<WATCard::FWATCard> usedWatCards;
+
     void main();
   public:
-    Groupoff( Printer &prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay );
+    Groupoff(Printer &prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupOffDelay);
     WATCard::FWATCard giftCard();
 };
 
-#endif
+#endif // __GROUPOFF_H__
