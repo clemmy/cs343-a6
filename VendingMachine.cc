98b36d6b77b671cfa2082b7796acba32e943a7d8
isdemus <isdemus@gmail.com>
2016-12-01 19:40:55 -0500

    small add to VendingMachine.cc

--- VendingMachine.cc
+++ VendingMachine.cc
@@ -1,9 +1,13 @@
 #include "VendingMachine.h"
 
-VendingMachine::VendingMachine(Printer &prt, NameServer &nameserver, unsigned int id, unsigned int sodaCost,
-                               unsigned int maxStockPerFlavour)
+VendingMachine::VendingMachine(Printer &prt, NameServer &nameserver, unsigned int id,
+															 unsigned int sodaCost, unsigned int maxStockPerFlavour)
   : printer(ptr), nameserver(nameserver), machineid(id), sodacost(sodaCost), maxstock(maxStockPerFlavour) {
   stockstatus = new size_t[3];
+	for (size_t index = 0; index < 3; index++) {
+		stockstatus = 0;
+	}
+
 }
 
 VendingMachine::~VendingMachine() {
@@ -11,10 +15,29 @@ VendingMachine::~VendingMachine() {
   printer.print(Printer::Kind::VendingMachine, 'F');
 }
 
-void VendingMachine::main() {
-  printer.print(Printer::Kind::VendingMachine, 'S');
+void VendingMachine::buy(Flavours flavour, WATCard &card) {
+
 }
 
-void VendingMachine::buy(Flavours flavour, WATCard &card) {
+unsigned int * VendingMachine::inventory() {
+	return stockstatus;
+}
+
+void VendingMachine::restocked() {
+
+}
+
+unsigned int VendingMachine::cost() {
+	return sodacost;
+}
+
+unsigned int VendingMachine::getId() {
+	return machineid;
+}
+
+void VendingMachine::main() {
+  printer.print(Printer::Kind::VendingMachine, 'S');
+	// Registering itself to the Name Server
+	nameserver.register(this);
 
 }
